dc2f0697c2b4abc8063ba2272978fe7e150b8d2f
Criss <cgutierrez.infor@gmail.com>
2024-05-17 15:24:41 -0400

    ready to prod

--- stream.py
+++ stream.py
@@ -79,7 +79,8 @@ def preparar_datos(col2, page_name):
                 with st.spinner('🚀 Buscando líneas de pedido en blueline...'):
                     run_playwright_script("download_report")
                     run_playwright_script("process_report")
-                    tabular_path = os.path.join(os.getcwd(), "formato_tabular.xlsx")
+
+                    tabular_path = os.path.join(temp_dir, "formato_tabular.xlsx")
                     df_tabular = pd.read_excel(tabular_path)
                     matched_df = df_tabular[df_tabular["FOLIO"].isin(folios_list)]
                     matched_df = matched_df.drop(columns=['TRACKID', 'ESTADO PORTAL', 'ESTADO SII'])
